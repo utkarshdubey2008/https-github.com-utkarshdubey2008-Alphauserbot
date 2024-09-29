c55d87b1eba610d671ea1d882f23215d0fc99cce
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-29 09:47:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # config.py
 
 # Your API ID and API Hash from https://my.telegram.org
-API_ID = 'YOUR_API_ID'  # Replace with your API ID
-API_HASH = 'YOUR_API_HASH'  # Replace with your API Hash
+API_ID = '29264175'  # Replace with your API ID
+API_HASH = '0a3e78a4cd29529769faf5fe588f6de7'  # Replace with your API Hash
 
 # Session string for logging in
-SESSION_STRING = 'YOUR_SESSION_STRING'  # Replace with your session string
+SESSION_STRING = '1BVtsOHgBu5aAc6oDfmcOKxoqFW5uwNsU_nIA3HUley29OUmzaTsRM2nogXUeCDOZjrm-pnW0JzAiuOFY2sjeRJSG6V9WvyF4PAdIqHwmONffEM0z2jAwc_IV0QT8uSE8SstmA_uR_e3dQ5qfBG3UBVkjR7DMTWCYq_3dxKiB61tjRu6SO1XjxpgtbSfH6OXiIt7SIyjQ5XPt9yKHid9V4dycYD02wRT5C0LHcI349Ujfry54M5teWHpNnrcMJKsBdtqLs544uOq3QeM5SQDRPSIfTW0m44yDbA8lZZpx0itNaoKzgOQZ9bAL0CQ0m6WLKmLt8Kfo2ghuNz2husZHRl2rvObgOQo='  # Replace with your session string
 
 # Optional: Define any other configurations
 # Example:
